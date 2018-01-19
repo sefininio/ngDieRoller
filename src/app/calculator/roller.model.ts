2df8cd43f92d46dec3c0104b1c36e14806d63690
Sefi Ninio <sefi.ninio@gmail.com>
2018-01-19 21:10:22 +0200

    Feature/presets service (#3)
    * presets service
    
    * code formatting

--- src/app/calculator/roller.model.ts
+++ src/app/calculator/roller.model.ts
@@ -1,28 +1,27 @@
-
 export class RollResult {
-    dieSides: number;
-    rolls: number[];
+  dieSides: number;
+  rolls: number[];
 
-    constructor(dieSides: number, rolls: number[]) {
-        this.dieSides = dieSides;
-        this.rolls = rolls;
-    }
+  constructor(dieSides: number, rolls: number[]) {
+    this.dieSides = dieSides;
+    this.rolls = rolls;
+  }
 }
 
 export class RollerModel {
-    resRolls: string[];
-    rolls: string[];
-    rollStats: RollResult[];
-    grandTotal: number;
+  resRolls: string[];
+  rolls: string[];
+  rollStats: RollResult[];
+  grandTotal: number;
 
-    constructor() {
-        this.resRolls = [];
-        this.rolls = [];
-        this.rollStats = [];
-        this.grandTotal = 0;
-    }
+  constructor() {
+    this.resRolls = [];
+    this.rolls = [];
+    this.rollStats = [];
+    this.grandTotal = 0;
+  }
 
-    public addRollSeries(dieSides: number, rolls: number[]) {
-        this.rollStats.push(new RollResult(dieSides, rolls));
-    };
+  public addRollSeries(dieSides: number, rolls: number[]) {
+    this.rollStats.push(new RollResult(dieSides, rolls));
+  };
 }
---
+++ src/app/persistance/presets.service.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@angular/core';
+import { range } from 'lodash';
+
+@Injectable()
+export class PresetsService {
+
+  public getPresets(): string[] {
+    return range(1, 21).map((item) => {
+      let preset = localStorage.getItem(`preset${item}`);
+
+      if (!preset) {
+        preset = `${item}d6`;
+      }
+
+      return preset;
+    });
+  }
+
+  public savePreset(preset: string, idx: number) {
+    localStorage.setItem(`preset${idx}`, preset);
+  }
+}
