89134ddb76bd66dd84ae8d2704922bdbe459dd6c
jsBlackBelt <sefi.ninio@gmail.com>
2018-01-20 02:55:04 +0200

    styling and presets
    1. Introduce Material Design
    2. Support for presets (default values for now)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
 import { RollerService } from './calculator/roller.service';
 import { PresetsService } from './persistance/presets.service';
@@ -8,29 +8,43 @@ import { PresetsService } from './persistance/presets.service';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'Die Roller';
-  toRoll = '';
+  toRoll = '1d6';
   repeat = 1;
   grandTotal = 0;
   rolls: string[] = [];
   resRolls: string[] = [];
+  presets: string[];
 
   constructor(private rollerService: RollerService,
               private presetService: PresetsService) {
   }
 
-  doRoll(form: NgForm) {
-    if (!this.toRoll || this.repeat < 1) {
+  ngOnInit() {
+    this.presets = this.presetService.getPresets();
+  }
+
+  submitRollForm(rollObj: any) {
+    this.doRoll(rollObj.toRoll, rollObj.repeat);
+  }
+
+  submitRollPreset(preset: string) {
+    this.doRoll(preset, 1);
+  }
+
+  doRoll(roll: string, repeat: number) {
+    if (!roll || repeat < 1) {
       return;
     }
+
     this.rollerService.clear();
     this.grandTotal = 0;
 
-    for (let i = 0; i < this.repeat; i++) {
-      this.rollerService.evaluate(this.toRoll);
+    for (let i = 0; i < repeat; i++) {
+      this.rollerService.evaluate(roll);
 
-      if (i + 1 < this.repeat) {
+      if (i + 1 < repeat) {
         this.rollerService.rollerModel.rolls.push('------------------\n');
         this.rollerService.rollerModel.resRolls.push('------------------\n');
       }
@@ -44,14 +58,5 @@ export class AppComponent {
     this.rolls = this.rollerService.rollerModel.rolls;
     this.resRolls = this.rollerService.rollerModel.resRolls;
   }
-
-  spread(input: string[]): string {
-    return input
-      .map(item => `${item}\n`)
-      .reduce((curr, item) => {
-        return curr + item;
-      }, '');
-  }
-
 
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,10 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
+import { AppMaterialModule } from './app.material.module';
 
 import { AppComponent } from './app.component';
 import { RollerService } from './calculator/roller.service';
@@ -15,6 +17,8 @@ import { PresetsService } from './persistance/presets.service';
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
+    AppMaterialModule,
     AppRoutingModule,
     FormsModule,
   ],
